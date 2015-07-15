199be7e33e70ca8416d8da8616144ffee52ebc35
Felipe Martín <fmartingr@gmail.com>
2015-07-15 17:37:35 +0200

    Added crappy setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='guild-empire-core',
+    version='0.1.0',
+    description='Guild empire core',
+    author='Guild empire core team',
+    author_email='guild-empire@googlegroups.com',
+    url='piweek',
+    packages=['actions', 'characters', 'games', 'guilds', 'places', 'storage'],
+    py_modules=['exceptions', 'setup', 'utils']
+)
